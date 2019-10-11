407263a224e3ec005ab1512e52884a4563694687
Ivan Dai <ivandaixivwork@gmail.com>
2019-10-10 19:17:13 -0700

    Featured CSS deleted for troubleshooting purposes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+    const elem = document.querySelector('.main-carousel');
+    const flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    wrapAround: true,
+    freeScroll: true
+    });
 });  // end of doc ready
